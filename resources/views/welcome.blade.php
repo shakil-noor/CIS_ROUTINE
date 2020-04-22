e89b7487d37bad6d3c05d0334f0a94edc40ce32a
shakil-noor <hossain.bd1182@gmail.com>
2020-04-22 15:18:56 +0600

    Change design on Welcome page. All login action will be happen from here

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,102 +1,43 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>CIS Routine</title>
+<head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <title>Class Routine Management System</title>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ asset('assets/admin/css/bootstrap.min.css') }}" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Custom Fonts -->
+    <link href="{{ asset('assets/admin/css/welcome.css') }}" rel="stylesheet" type="text/css">
 
-            .position-ref {
-                position: relative;
-            }
+</head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<body id="page-top">
 
-            .content {
-                text-align: center;
-            }
+<!-- Header -->
+<header class="masthead d-flex" style="background-image: url('{{ asset('image/bg-masthead.jpg')}}');">
+    <div class="container text-center my-auto">
+        <h1 class="mb-1">Welcome</h1>
+        <h3 class="mb-5">
+            <em>Daffodil International University</em>
+        </h3>
 
-            .title {
-                font-size: 84px;
-            }
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="{{ route('login') }}">Admin</a>
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Coordinator</a>
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Teacher</a>
+        <h4 class="mb-5">
+            <em>Powered by Department of CIS</em>
+        </h4>
+    </div>
+    <div class="overlay"></div>
+</header>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+</body>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Admin Login</a>
-                        <a href="{{ route('login') }}">Teacher Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="{{ route('admin.dashboard') }}">dashboard</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
 </html>
